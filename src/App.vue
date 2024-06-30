efa19e4674f286dc08f8d2a41378df9eac6b701e
yuying <yuying10268@navinfo.com>
2024-06-30 12:50:48 +0800

    yy

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import CarouselCom from "./components/carousel.vue"
+</script>
+
+<template>
+  <div>
+    <CarouselCom></CarouselCom>
+    <div>
+      test github actions gitFlow
+    </div>
+  </div>
+</template>
